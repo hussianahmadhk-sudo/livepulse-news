0e76ef2b369c8d59d9516ca8cbdaf7d340abfc3f
hussianahmadhk-sudo <hussianahmadhk@gmail.com>
2025-11-06 18:07:57 +0530

    Delete livepulse-news directory

--- livepulse-news/pages/index.js
+++
@@ -1,1 +0,0 @@
-﻿export default function Home(){return <div style={{fontFamily:"sans-serif",padding:"2em"}}><h1>📰 LivePulse News</h1><p>Your site is working! Next: add NEWSAPI_KEY in Vercel settings.</p></div>}
