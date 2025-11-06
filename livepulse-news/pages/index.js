8f967e3ccbc0aa0c7e81c7003272aea62e119945
hussianahmadhk-sudo <hussianahmadhk@gmail.com>
2025-11-06 17:34:51 +0530

    Add files via upload

---
+++ livepulse-news/pages/index.js
@@ -0,0 +1,1 @@
+﻿export default function Home(){return <div style={{fontFamily:"sans-serif",padding:"2em"}}><h1>📰 LivePulse News</h1><p>Your site is working! Next: add NEWSAPI_KEY in Vercel settings.</p></div>}
